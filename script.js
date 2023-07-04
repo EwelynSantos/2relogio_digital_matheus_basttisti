79f444d92e48567e62667dc7d58e3ec9aff135cd
EwelynSantos <ewelyn.santos98@gmail.com>
2023-07-04 16:24:01 -0300

    subindo projeto

---
+++ script.js
@@ -0,0 +1,18 @@
+const hoursElement= document.querySelector(".hours")
+const minutesElement=document.querySelector(".minutes")
+const secondsElement= document.querySelector(".seconds")
+
+function clock(){
+  
+  const now= new Date()
+
+  const hours = now.getHours().toString().padStart(2,"0")
+  const minutes = now.getMinutes().toString().padStart(2,"0")
+  const seconds = now.getSeconds().toString().padStart(2,"0")
+
+hoursElement.textContent=hours
+minutesElement.textContent=minutes
+secondsElement.textContent=seconds
+}
+
+setInterval(clock, 1000)
